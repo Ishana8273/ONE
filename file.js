acbf9b1a8366a70577b636d4b98780d952c1e07d
Ishana82 <malikjaivindra91@gmail.com>
2026-01-10 19:52:15 +0530

    added another function to file.js

--- file.js
+++ file.js
@@ -2,3 +2,14 @@ function myFunction() {
     console.log("Hello World!");
 }
 myFunction();
+
+function anotherFunction() {
+    return 42;
+}
+console.log(anotherFunction());
+
+const arrowFunction = () => {
+    return "Arrow Function!";
+}
+console.log(arrowFunction());
+console.log("This is a recently edited file.");
